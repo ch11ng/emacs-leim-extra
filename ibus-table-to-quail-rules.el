0f75adf019346c757698d2de18abebc01596e68f
Chris Feng <chris.w.feng@gmail.com>
2015-07-21 09:43:03 +0800

    Fix words order and improve conversion speed

--- ibus-table-to-quail-rules.el
+++ ibus-table-to-quail-rules.el
@@ -28,60 +28,50 @@
 
 ;;; Code:
 
-(defun ibus-table->quail-rules (ibus-table)
-  "Return quail rules from IBus table data."
-  (let ((data
-         (with-temp-buffer
-           (message "Reading %s..." ibus-table)
-           (insert-file-contents ibus-table)
-           (goto-char (point-min))
-           (delete-region (point-min) (search-forward "\nBEGIN_TABLE\n"))
-           (goto-char (point-max))
-           (delete-region (search-backward "\nEND_TABLE\n") (point-max))
-           (mapcar 'split-string (split-string (buffer-string) "\n" t))))
-        (hash (make-hash-table :test 'equal))
-        rules)
-    (message "Converting...")
-    (mapc (lambda (i)
-            (let ((key (elt i 0))
-                  (translation (elt i 1))
-                  (frequency (string-to-int (elt i 2))))
-              (when (= 1 (length translation))
-                (setq translation (elt translation 0)))
-              (dotimes (j (1- (length key)))
-                (let ((key* (substring key 0 (1+ j))))
-                  (puthash key*
-                           (nconc (gethash key* hash)
-                                  `((,translation . ,frequency)))
-                           hash)))
-              (puthash key (nconc (gethash key hash) (list translation))
-                       hash)))
-          data)
-    (message "Finishing...")
-    (maphash (lambda (key val)
-               (setq rules
-                     (nconc rules
-                            `((,key
-                               ,(let ((translation
-                                       (mapcar (lambda (i)
-                                                 (if (listp i)
-                                                     (car i)
-                                                   i))
-                                               (sort val
-                                                     (lambda (a b)
-                                                       (if (nlistp a)
-                                                           t
-                                                         (if (nlistp b)
-                                                             nil
-                                                           (> (cdr a)
-                                                              (cdr b)))))))))
-                                  (if (and (= 1 (length translation))
-                                           (not (stringp (car translation))))
-                                      (car translation)
-                                    (vconcat translation))))))))
-             hash)
-    rules))
-
-(print (ibus-table->quail-rules (car argv)))
+(let ((data
+       (with-temp-buffer
+         (message "Reading %s..." (car argv))
+         (insert-file-contents (car argv))
+         ;; Get text between BEGIN_TABLE and END_TABLE
+         (goto-char (point-min))
+         (delete-region (point-min) (search-forward "\nBEGIN_TABLE\n"))
+         (goto-char (point-max))
+         (delete-region (search-backward "\nEND_TABLE\n") (point-max))
+         ;; Convert text to a list (rows) of another list (columns)
+         (mapcar 'split-string (split-string (buffer-string) "\n" t))))
+      (hash (make-hash-table :test 'equal)))
+  (message "Converting...")
+  (mapc (lambda (i)
+          (let* ((key (elt i 0))
+                 (translation (elt i 1))
+                 (frequency (string-to-int (elt i 2)))
+                 (key-length (length key)))
+            (dotimes (j key-length)
+              (let ((key* (substring key 0 (1+ j))))
+                (puthash key*
+                         (nconc (gethash key* hash)
+                                `((,translation ,frequency ,(- key-length j))))
+                         hash)))))
+        data)
+  (message "Finishing...")
+  (maphash (lambda (key val)
+             (prin1
+              `(,key
+                ,(let ((translation
+                        (delete-dups
+                         (mapcar 'car
+                                 (sort val
+                                       (lambda (a b)
+                                         (let ((a-group (elt a 2))
+                                               (b-group (elt b 2)))
+                                           (if (/= a-group b-group)
+                                               (< a-group b-group)
+                                             (> (cadr a) (cadr b))))))))))
+                   (if (and (= 1 (length translation))
+                            (= 1 (length (car translation))))
+                       (car translation)
+                     (vconcat translation)))))
+             (terpri))
+           hash))
 
 ;;; ibus-table-to-quail-rules.el ends here
